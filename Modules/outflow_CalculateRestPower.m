146a8461c355a91ae8b20e863723603fc0cc7dcf
NSuer <122294714+NSuer@users.noreply.github.com>
2023-09-13 09:21:28 -0400

    Revert "Update outflow_CalculateRestPower.m"
    This reverts commit b15fab661455bbb0fa690cf32331b41d2ae85dcc.

--- Modules/outflow_CalculateRestPower.m
+++ Modules/outflow_CalculateRestPower.m
@@ -19,21 +19,16 @@ classdef outflow_CalculateRestPower < SignalFlowSuperClass
             args.num_gpuOn = 0;
             args.num_duration = 60;
             args.num_offset = 0;
-            args.num_window = 2; 
+            args.num_window = 2;
+            args.char_outputdir = obj.fileIoVar;
             args.bandDefs = {'delta', 2, 3.5; 'theta', 3.5, 7.5; 'alpha1', 8, 10;
                             'alpha2', 10.5, 12.5; 'beta', 13, 30; 'gamma1', 30, 55;
                             'gamma2', 65, 80; 'epsilon', 81, 120; };
             args.log_useParquet = false;
-            
-            if ~isempty(obj.fileIoVar) %Will be printed to output directory 
-                args.char_outputdir = obj.fileIoVar;
-                [EEG,args.results] = eeg_htpCalcRestPower(EEG, 'gpuOn',args.num_gpuOn,'duration',args.num_duration,...
+
+            [EEG,args.results] = eeg_htpCalcRestPower(EEG, 'gpuOn',args.num_gpuOn,'duration',args.num_duration,...
                 'offset',args.num_offset,'window',args.num_window,'outputdir', args.char_outputdir,'bandDefs',args.bandDefs,'useParquet',args.log_useParquet);
-            else %Will be printed to input directory
-                [EEG,args.results] = eeg_htpCalcRestPower(EEG, 'gpuOn',args.num_gpuOn,'duration',args.num_duration,...
-                'offset',args.num_offset,'window',args.num_window,'bandDefs',args.bandDefs,'useParquet',args.log_useParquet);
-            end
-            
+
             %Parameters and run history is stored in EEG.etc.SignalFlow.History field in EEG structure
             [args.QADataPost] = util_GetQAData(EEG);
             EEG = obj.HistoryTable(EEG, args);
