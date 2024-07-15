64cf9d30b5ebcc0845c2a00dac5db7d32e54ceeb
NSuer <natesuer@example.com>
2024-07-15 15:04:59 +0000

    rename

---
+++ Modules/inflow_ImportMeaXDAT.m
@@ -0,0 +1,42 @@
+classdef inflow_ImportMeaXDAT < SignalFlowSuperClass
+%% Disclaimer:
+%  This file is part of the Cincinnati Childrens Brain Lab SignalFlowEEG Pipeline
+%  
+%  Please see http://github.com/cincibrainlab
+%
+%% Contact:
+%  https://github.com/cincibrainlab/SignalFlowEEG/issues
+    methods
+        function obj = inflow_ImportMeaXDAT(varargin)
+           
+            % Define Custom Flow Function
+            setup.flabel = 'Import MEA XDAT File';
+            setup.flowMode = 'inflow';
+
+            % Construction Function
+            obj = obj@SignalFlowSuperClass(setup, varargin{:});
+        end
+
+        function EEG = run(obj, varargin)
+            % run() - Process the EEG data.           
+            % Signal Processing Code Below
+            args.char_filepath = obj.fileIoVar;
+            args.char_netType = 'MEAXDAT';
+
+            % Get the folder path, file name, and file extension
+            [folderPath, ~, fileExtension] = fileparts(args.char_filepath);        
+            % Add the folder path to the search path
+            addpath(folderPath);
+            % Load the file using pop_loadset if it has a .set extension
+            if strcmp(fileExtension, '.xdat')
+                EEG = util_htpImportAndRemapMea(args.char_filepath);
+                %Parameters and run history is stored in EEG.etc.SignalFlow.History field in EEG structure
+                EEG = obj.HistoryTable(EEG, args);
+            else 
+                EEG = [];
+            end
+            [args.QADataPost] = util_GetQAData(EEG);
+            EEG = obj.HistoryTable(EEG, args);
+        end
+    end
+end
