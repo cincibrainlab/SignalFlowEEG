be6f69d8da47720ea7fce30ed8718e227a462c25
Gavin Gammoh <gammohgb@mail.uc.edu>
2024-08-05 12:16:28 -0400

    cleanup

--- playground.m
+++
@@ -1,5 +0,0 @@
-
-clear all;
-close all;
-args.char_filepath = 'C:\Users\Gam9LG\Documents\TestData\MeaTroubleshooting\080224\allego_1__uid0802-12-56-20_data.xdat';
-eeg = util_allegoXDatAddEvents_chirp(args.char_filepath);
